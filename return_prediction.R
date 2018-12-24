4c5e4ae0f2da99b8f32a0dfcba53af243a4309a8
Your Name <you@example.com>
2018-12-24 13:43:04 +0100

    with new function for missing level interpolation, not heavily tested yet

--- return_prediction.R
+++ return_prediction.R
@@ -1,151 +1,222 @@
 # This function keeps only those columns whose id are given as vector.
 # Also keeps columns whose ids are superstring of given id.
 keep_cols <- function(data, keep=NULL){
-   if (is.null(keep))
-      return(data);
-
-   to_keep <- rep(FALSE, ncol(data));
-   for (k in keep)
-      to_keep <- to_keep | grepl(k, colnames(data));
-
-   ret <- data[,to_keep];
-   ret <- ret[,order(colnames(ret))];
-   return(ret);
+  if (is.null(keep))
+    return(data);
+  
+  to_keep <- rep(FALSE, ncol(data));
+  for (k in keep)
+    to_keep <- to_keep | grepl(k, colnames(data));
+  
+  ret <- data[,to_keep];
+  ret <- ret[,order(colnames(ret))];
+  return(ret);
 }
 
 # This function merges data frames from list.
 merge_data <- function(data, keep=NULL, drop_cols=FALSE){
-   ret <- NULL;
-   for (i in 1:length(data)){
-      if (is.null(keep) || names(data)[i] %in% keep){
-         datum <- data[[i]];
-         if (drop_cols)
-            datum <- datum[,!is.na(datum[nrow(datum),])]
-
-         index <- which(grepl("date", colnames(datum)));
-         if (length(index) == 1){
-            if (is.null(ret)){
-               ret <- datum;
-               byx <- colnames(datum)[index];
-            } else {
-               byy <- colnames(datum)[index];
-               ret <- merge(x=ret, y=datum, by.x=byx, by.y=byy);
-            }
-         }
+  ret <- NULL;
+  for (i in 1:length(data)){
+    if (is.null(keep) || names(data)[i] %in% keep){
+      datum <- data[[i]];
+      if (drop_cols)
+        datum <- datum[,!is.na(datum[nrow(datum),])]
+      
+      index <- which(grepl("date", colnames(datum)));
+      if (length(index) == 1){
+        if (is.null(ret)){
+          ret <- datum;
+          byx <- colnames(datum)[index];
+        } else {
+          byy <- colnames(datum)[index];
+          ret <- merge(x=ret, y=datum, by.x=byx, by.y=byy);
+        }
       }
-   }
-
-   return(ret);
+    }
+  }
+  
+  return(ret);
 }
 
 # This function reads files from given directory.
 read_dir <- function(dir){
-   files <- list.files(dir, pattern="*.csv");
-
-   ret <- list();
-   for (file in files){
-      datum <- read.csv(file=paste(dir, file, sep="/"));
-      colnames(datum) <- paste(file, colnames(datum), sep=".");
-      ret <- append(ret, list(datum));
-   }
-   names(ret) <- files;
-
-   return(ret);
+  files <- list.files(dir, pattern="*.csv");
+  
+  ret <- list();
+  for (file in files){
+    datum <- read.csv(file=paste(dir, file, sep="/"));
+    colnames(datum) <- paste(file, colnames(datum), sep=".");
+    ret <- append(ret, list(datum));
+  }
+  names(ret) <- files;
+  
+  return(ret);
 }
 
 # This functions computes the open and close price.
 make_open_close <- function(data, number_of_days=7, date="DATE", price="PRICE"){
-   x <- data;
-
-   # extrahiere ersten und letzten Tag der Woche
-   if (number_of_days > 1){
-      x <- cbind(x, "WEEK_DAY"=((1:nrow(x))%%number_of_days));
-      open <- x[x$WEEK_DAY==1, price];
-      close <- x[x$WEEK_DAY==0, price];
-      open_date <- x[x$WEEK_DAY==1, date];
-      close_date <- x[x$WEEK_DAY==0, date];
-   } else {
-      open <- x[,price];
-      close <- x[,price];
-      open_date <- x[,date];
-      close_date <- x[,date];
-   }
-
-   # passe die Länge der Vektoren an sodass es für jeden Tag eine Entsprechung gibt, einer der Fälle ist glaube ich unnötig
-   if (length(open) > length(close)) {open <- open[1:length(close)]; open_date <- open_date[1:length(close)];};
-   if (length(open) < length(close)) {close <- close[1:length(open)]; close_date <- close_date[1:length(open)];};
-
-   return(data.frame("OPEN_DATE"=open_date, "CLOSE_DATE"=close_date, "OPEN_PRICE"=open, "CLOSE_PRICE"=close));
+  x <- data;
+  
+  # extrahiere ersten und letzten Tag der Woche
+  if (number_of_days > 1){
+    x <- cbind(x, "WEEK_DAY"=((1:nrow(x))%%number_of_days));
+    open <- x[x$WEEK_DAY==1, price];
+    close <- x[x$WEEK_DAY==0, price];
+    open_date <- x[x$WEEK_DAY==1, date];
+    close_date <- x[x$WEEK_DAY==0, date];
+  } else {
+    open <- x[,price];
+    close <- x[,price];
+    open_date <- x[,date];
+    close_date <- x[,date];
+  }
+  
+  # passe die Länge der Vektoren an sodass es für jeden Tag eine Entsprechung gibt, einer der Fälle ist glaube ich unnötig
+  if (length(open) > length(close)) {open <- open[1:length(close)]; open_date <- open_date[1:length(close)];};
+  if (length(open) < length(close)) {close <- close[1:length(open)]; close_date <- close_date[1:length(open)];};
+  
+  return(data.frame("OPEN_DATE"=open_date, "CLOSE_DATE"=close_date, "OPEN_PRICE"=open, "CLOSE_PRICE"=close));
 }
 
 # This function computes the return from matrix with corresponding columns.
 make_return <- function(data){
-   return((data$CLOSE_PRICE - data$OPEN_PRICE)/data$OPEN_PRICE);
+  return((data$CLOSE_PRICE - data$OPEN_PRICE)/data$OPEN_PRICE);
 }
 
 # Some helpful function.
 test_levels <- function(data, id=NULL){
-   if (!is.null(id))
-      print(id);
-   bar <- table(data);
-   print(bar[bar == 1]);
+  if (!is.null(id))
+    print(id);
+  bar <- table(data);
+  print(bar[bar == 1]);
 }
 
 # This function takes the interval of a given factor, [min(F), max(F)],
 # and cuts it into the given number of subintervals of same size. Each
 # subinterval represents a level in the corresponding new factor.
 make_breaks <- function(data, breaks=5, ignore=c()){
-   ret <- data;
-   for (i in 3:ncol(ret))
-      if (!(colnames(ret)[i] %in% ignore))
-         ret[,i] <- cut(ret[,i], breaks=breaks);
-
-   return(ret);
+  ret <- data;
+  for (i in 3:ncol(ret))
+    if (!(colnames(ret)[i] %in% ignore))
+      ret[,i] <- cut(ret[,i], breaks=breaks);
+    
+    return(ret);
 }
 
 # This function creates the model formula.
 make_formula <- function(data, y=NULL, ignore=c()){
-   if (is.null(y))
-      stop("id for y-variable missing");
+  if (is.null(y))
+    stop("id for y-variable missing");
+  
+  ret <- paste(setdiff(colnames(data), c(y, ignore)), collapse=" + ");
+  ret <- paste(y, ret, sep=" ~ ");
+  
+  return(ret);
+}
 
-   ret <- paste(setdiff(colnames(data), c(y, ignore)), collapse=" + ");
-   ret <- paste(y, ret, sep=" ~ ");
-
-   return(ret);
+# This function returns the closest already used level for the given column 
+get_closest_level <- function(column_name, shifted_return, to_predict){
+  # table containing the number a level was used
+  lvl_usage_table <- table(shifted_return[,column_name]);
+  
+  # get levels for given column
+  lvls <- levels(to_predict[[column_name]]);
+  ids <- lvls;
+  lvls <- gsub("\\(", "", lvls);
+  lvls <- gsub("\\]", "", lvls);
+  lvls <- strsplit(lvls, ",");
+  
+  lower <- sort(as.numeric(unlist(lapply(lvls, function(x){return(x[1])}))));
+  names(lower) <- ids;
+  
+  # get missing level
+  vals <- as.character(to_predict[[column_name]]);
+  id <- vals;
+  vals <- gsub("\\(", "", vals);
+  vals <- gsub("\\]", "", vals);
+  vals <- as.numeric(strsplit(vals, ",")[[1]][1]);
+  
+  names(vals) <- id;
+  
+  starti <- which(names(lower) %in% names(vals));
+  
+  # find first level above, which was used in fit
+  upi <- Inf;
+  for (i in starti:length(lower)){
+    lvlid <- names(lower)[i];
+    if (lvl_usage_table[lvlid] > 0){
+      upi <- i;
+      break;
+    }
+  }
+  
+  # find first level below, which was used in fit
+  downi <- Inf;
+  for (i in starti:1){
+    lvlid <- names(lower)[i];
+    if (lvl_usage_table[lvlid] > 0){
+      downi <- i
+      break;
+    }
+  }
+  
+  # if both are infinite, there is something going wrongly
+  if (is.infinite(upi) && is.infinite(downi))
+    stop("there is something going wrongly");
+  
+  # if the upper index is closer to the start index return corresponding level
+  if ((upi - starti)^2 < (downi - starti)^2)
+    return(names(lower)[upi]);
+  
+  # otherwise return the level which is below
+  return(names(lower)[downi]);
 }
 
 # This function computes return prediction for the coming week.
-make_prediction <- function(data, price_id=NULL, shift=1, breaks=5, ignore=c()){
-   if (is.null(price_id))
-      stop("price_id missing");
-
-   breaked_return <- make_breaks(data, breaks=breaks, ignore=price_id);
-
-   shifted_return <- breaked_return;
-   shifted_return[1:(length(breaked_return[,price_id])-shift), price_id] <- breaked_return[(shift+1):length(breaked_return[,price_id]), price_id];
-   shifted_return <- shifted_return[1:(length(breaked_return[,price_id])-shift),];
-
-   formula <- make_formula(shifted_return, y=price_id, ignore=ignore);
-
-   shifted_fit <- lm(formula, data=shifted_return);
-
-   to_predict <- breaked_return[(nrow(breaked_return)-shift+1):nrow(breaked_return),];
-   to_predict[,price_id] <- NA;
-
-   # were levels for prediction used in fit?
-   b <- rep(TRUE,3); for (i in 4:length(to_predict)){b <- c(b, to_predict[1,colnames(to_predict)[i]] %in% shifted_return[,colnames(to_predict)[i]])}
-   if (sum(!b) > 0)
-      cat(paste("New levels: ", paste(colnames(to_predict)[!b], collapse=", "), "\n", sep=""));
-
-   # replace non-existing factors with last one in shifted_R
-   # actually it should be better to use factor from fit, which is closest
-   to_predict[1,!b] <- shifted_return[nrow(shifted_return),!b];
-
-   prediction <- predict(shifted_fit, to_predict);
-
-   ret <- list(prediction, to_predict, breaked_return, shifted_return, shifted_fit);
-   names(ret) <- c("PREDICTION", "TO_PREDICT", "BREAKED_RETURN", "SHIFTED_RETURN", "SHIFTED_FIT");
-   return(ret);
+make_prediction <- function(data, price_id=NULL, shift=1, breaks=5, ignore=c(), simple=FALSE){
+  if (is.null(price_id))
+    stop("price_id missing");
+  
+  breaked_return <- make_breaks(data, breaks=breaks, ignore=price_id);
+  
+  shifted_return <- breaked_return;
+  shifted_return[1:(length(breaked_return[,price_id])-shift), price_id] <- breaked_return[(shift+1):length(breaked_return[,price_id]), price_id];
+  shifted_return <- shifted_return[1:(length(breaked_return[,price_id])-shift),];
+  
+  formula <- make_formula(shifted_return, y=price_id, ignore=ignore);
+  
+  shifted_fit <- lm(formula, data=shifted_return);
+  
+  to_predict <- breaked_return[(nrow(breaked_return)-shift+1):nrow(breaked_return),];
+  to_predict[,price_id] <- NA;
+  
+  # were levels for prediction used in fit?
+  b <- rep(TRUE,3); for (i in 4:length(to_predict)){b <- c(b, to_predict[1,colnames(to_predict)[i]] %in% shifted_return[,colnames(to_predict)[i]])}
+  if (sum(!b) > 0)
+    cat(paste("New levels: ", paste(colnames(to_predict)[!b], collapse=", "), "\n", sep=""));
+  # save to_predict row for further analysis if something seems wrong
+  original_to_predict <- to_predict;
+  
+  if (simple){
+    # replace non-existing factors with last one in shifted_R
+    # actually it should be better to use factor from fit, which is closest
+    to_predict[1,!b] <- shifted_return[nrow(shifted_return),!b];
+  } else {
+    cnames <- colnames(to_predict)[!b];
+    
+    for (n in cnames){
+      replacement <- get_closest_level(n, shifted_return, to_predict);
+      cat(paste("Replacing level ", as.character(to_predict[[n]]), " of factor ", n, " with ", replacement, "\n", sep=""));
+      to_predict[1,n] <- replacement;
+    }
+  }
+  
+  # predict
+  prediction <- predict(shifted_fit, to_predict);
+  
+  ret <- list(prediction, to_predict, breaked_return, shifted_return, shifted_fit, original_to_predict, b);
+  names(ret) <- c("PREDICTION", "TO_PREDICT", "BREAKED_RETURN", "SHIFTED_RETURN", "SHIFTED_FIT", "ORIGINAL_TO_PREDICT", "USED_LEVELS");
+  return(ret);
 }
 
 # This functions computes return prediction for the coming week and plots it. 
@@ -156,8 +227,9 @@ doIt <- function(breaks=15,
                  assets=c("btc", "ltc", "eth", "xrp", "doge", "usdt", "gold", "sp500"),
                  type=c("price", "value", ".txVolume.", "marketcap"),
                  old=c(),
-                 back=0){
-
+                 back=0,
+                 simple=FALSE){
+  
   # read coinmetrics data from directory
   x <- read_dir(dir);
   
@@ -192,7 +264,7 @@ doIt <- function(breaks=15,
   return <- return[1:(nrow(return)-back),];
   
   # make prediction
-  return_prediction <- make_prediction(return, price_id=price_id, shift=1, breaks=breaks, ignore=c("OPEN_DATE", "CLOSE_DATE"));
+  return_prediction <- make_prediction(return, price_id=price_id, shift=1, breaks=breaks, ignore=c("OPEN_DATE", "CLOSE_DATE"), simple=simple);
   prediction <- return_prediction$PREDICTION;
   breaked_return <- return_prediction$BREAKED_RETURN;
   shifted_return <- return_prediction$SHIFTED_RETURN;
@@ -245,7 +317,7 @@ doIt <- function(breaks=15,
   return(return_prediction);
 }
 
-doIt(old=c(0.06927308, -0.108199108918301, 0.15401893716921));
+ret <- doIt(old=c(0.06927308, -0.108199108918301, 0.15401893716921), simple=TRUE);
 
 # debugging
 a <- doIt(back=1);
